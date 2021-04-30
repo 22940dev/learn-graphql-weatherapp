1884c3cc7d9e718f28571d55efb90aac5ac5e359
GeoBrodas <geobro2310@gmail.com>
2021-04-30 20:34:43 +0530

    Styling part 1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { GET_WEATHER_QUERY } from '../graphql/Queries'
+import { FiSearch } from 'react-icons/fi'
+import { WiDegrees } from 'react-icons/wi'
+import {
+  Spinner,
+  Input,
+  Stack,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableCaption,
+  VStack,
+  StackDivider,
+  HStack,
+  IconButton,
+} from '@chakra-ui/react'
 
 export default function Dashboard() {
   const [result, setResult] = useState('')
   const [value, setValue] = useState('')
+  //const [message, setMessage] = useState('')
   const [getWeather, { loading, data, error }] = useLazyQuery(
     GET_WEATHER_QUERY,
     {
@@ -14,42 +31,94 @@ export default function Dashboard() {
 
   if (error) return <h1>No City found!</h1>
   if (data) {
-    console.log(data)
-    console.log(loading)
+    //console.log(data)
+    //console.log(error, loading)
   }
 
+  // function Errormessage(data) {
+  //   const nullvalue = data.getCityByName
+  //   setMessage(nullvalue)
+  // }
+
   return (
-    <div className="App parent">
+    <VStack>
       <h1>Search your city</h1>
-      <div className="children">
-        <input
+      <HStack>
+        <Input
           type="text"
           placeholder="Enter city name"
           onChange={(event) => setResult(event.target.value)}
         />
-        <button
+        <IconButton
+          icon={<FiSearch />}
           onClick={() => {
             setValue(result)
             getWeather()
+            //Errormessage()
           }}
         >
           Search
-        </button>
-      </div>
+        </IconButton>
+      </HStack>
+      <Stack>
+        {loading && (
+          <>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          </>
+        )}
+      </Stack>
       <div>
         {data && (
           <>
-            <p>{data.getCityByName.name}</p>
-            <p>
-              {Math.floor(data.getCityByName.weather.temperature.actual - 273) +
-                ' '}
-              deg C
-            </p>
-            <p>{data.getCityByName.weather.summary.description}</p>
-            <p>{data.getCityByName.weather.wind.speed + ' '}mph</p>
+            <VStack
+              divider={<StackDivider />}
+              borderColor="gray.100"
+              borderWidth="2px"
+              p="4"
+              borderRadius="lg"
+              w="100%"
+              maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}
+              alignItems="stretch"
+            >
+              <Table variant="simple">
+                <TableCaption>
+                  Weather in {data.getCityByName.name} right now
+                </TableCaption>
+
+                <Tbody>
+                  <Tr>
+                    <Td>Temperature</Td>
+
+                    <Td isNumeric>
+                      {Math.floor(
+                        data.getCityByName.weather.temperature.actual - 273
+                      ) + ' '}
+                      Deg C
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Feels like</Td>
+                    <Td>{data.getCityByName.weather.summary.description}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Wind speed (mph)</Td>
+
+                    <Td isNumeric>
+                      {data.getCityByName.weather.wind.speed + ' '}
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </VStack>
           </>
         )}
       </div>
-    </div>
+    </VStack>
   )
 }
